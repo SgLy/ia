44c0106ab22cce0ab891c8819b77ea0a62a9618a
SgLy <775150558@qq.com>
2018-05-17 09:39:34 +0800

    function class

--- backend/algorithm/main.cpp
+++ backend/algorithm/main.cpp
@@ -1,22 +1,14 @@
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <memory>
-#include <windows.h>
-
-using std::string;
-using std::make_unique;
-using std::unique_ptr;
+#include <vector>
+#include "functions.hpp"
 
 int main()
 {
-    for (int i = 0; i < 50; ++i) {
-        Sleep(100); // simulate calculation
-        auto filename = make_unique<char[]>(64);
-        sprintf(filename.get(), "tmp/%d.out", i);
-        auto f = unique_ptr<FILE, decltype(&fclose)>(
-            fopen(filename.get(), "w"), &fclose);
-        fprintf(f.get(), "{ \"data\": %d }", i);
-    }
+    std::vector<double> x;
+    x.resize(10);
+    for (int i = 0; i < 10; ++i)
+        x[i] = i;
+
+    Function f(1, 10);
+    double t = f.f(x);
     return 0;
 }
