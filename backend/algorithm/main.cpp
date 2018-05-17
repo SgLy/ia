2ccde2118dbb129eefc34af39bd656ff6714f73f
SgLy <775150558@qq.com>
2018-05-18 00:43:51 +0800

    front-back sync

--- backend/algorithm/main.cpp
+++ backend/algorithm/main.cpp
@@ -8,6 +8,13 @@ int main()
 
     Function f(func_num, D);
     printf("%.5lf\n", f(x));
-    f.dump("../../frontend/src/data.json", 0.25);
+    // f.dump("../../frontend/src/data.json", 0.25);
+    for (int i = 0; i < 50; ++i) {
+        char filename[256];
+        sprintf(filename, "tmp/%di.json", i);
+        FILE * f = fopen(filename, "w");
+        fprintf(f, "{ \"data\": %d }\n", i);
+        fclose(f);
+    }
     return 0;
 }
